3b10af00b8c94932c5745312d8b2946ccb120f3b
Thomas-T3 <tluerkens@gmx.de>
2017-03-19 20:24:47 +0100

    Add files via upload

---
+++ Fehlerspeicher_2017-2.ino
@@ -0,0 +1,16 @@
+#include <Wire.h>
+
+//#include "NewSoftwareSerial.h"
+#include "pgmspace.h"
+
+void setup(){
+  Serial.begin(19200);
+  Serial1.begin(9600);
+   //Serial1.write("Hallo Serial1");
+  OBD_setup();
+}
+void loop(){
+   
+  
+  OBD_loop();
+}
